4bb0dee313216a3f642f679c2be581e2ba105fb8
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-08-04 20:54:36 +0100

    feat: implement `/status` api

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+# Declare environmental variables
+HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
+PORT = os.getenv('HBNB_API_PORT', 5000)
+
+# Create an instance of flask
+app = Flask(__name__)
+
+# Register the blueprint `app_views` to app
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_storage():
+    """Close DB session"""
+    storage.close()
+
+if __name__ == "__main__":
+    """Run Flask app"""
+    app.run(host=HOST, port=PORT)
