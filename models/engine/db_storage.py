41badc350987ee83148916454bb1865dc90f728e
Normich ITOUA <nitoua.dev@gmail.com>
2024-09-25 23:02:59 +0100

    Update tasks

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,17 +76,31 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """Returns the object based on the class and its ID"""
-        try:
-            return self.__session.query(classes[cls]).filter_by(id=id).one()
-        except Exception as e:
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
             return None
 
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
+
     def count(self, cls=None):
-        """Counts the objects in storage matching the given class"""
-        if cls is not None:
-            return self.__session.query(classes[cls]).count()
-        return sum(
-            self.__session.query(classes[k]).count()
-            for k in classes if k != "BaseModel"
-        )
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
